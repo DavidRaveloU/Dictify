36bff9e790610d194efeb04b3da66226c85bb6f7
DavidRaveloU <dravelo@unicesar.edu.co>
2025-03-20 14:52:16 -0500

    Refactor error handling in fetchWordDetails function to improve clarity and maintainability

--- app/api/dictionary.ts
+++ app/api/dictionary.ts
@@ -5,21 +5,19 @@ const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 export async function fetchWordDetails(word: string): Promise<WordDetails | null> {
   try {
     const response = await fetch(`${API_URL}/${word}`);
-    // Verifica si la respuesta no es exitosa
     if (!response.ok) {
       throw new Error(`Error: ${response.status} - ${response.statusText}`);
     }
 
     const data = await response.json();
 
-    // Verifica si la respuesta es un array y tiene al menos un elemento
     if (!Array.isArray(data) || data.length === 0) {
       throw new Error('No data found');
     }
 
-    return data[0]; // Devuelve el primer elemento del array
+    return data[0];
   } catch (error) {
-    console.error('Error fetching word details:', error);
+    //console.error('Error fetching word details:', error);
     return null;
   }
 }
