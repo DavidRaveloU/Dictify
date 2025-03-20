e80486d637865a5e9468340c22de5ca925f99c8f
DavidRaveloU <dravelo@unicesar.edu.co>
2025-03-20 14:52:10 -0500

    Enhance loading indicator in Details screen for better visibility and user experience

--- app/details.tsx
+++ app/details.tsx
@@ -70,7 +70,9 @@ export default function Details() {
         }}
       />
       {loading ? (
-        <ActivityIndicator size="large" />
+        <View className="mt-7 flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#2563eb" />
+        </View>
       ) : !wordDetails ? (
         <Text>Word not found.</Text>
       ) : (
