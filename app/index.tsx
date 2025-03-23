b3f3dcf0478e3184e91ea23d71b750960cf79e64
DavidRaveloU <dravelo@unicesar.edu.co>
2025-03-23 17:22:56 -0500

    Refactor Home component to integrate LoadingIndicator, SearchBar, and WordCard for improved functionality and user experience

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Link, Stack, useRouter } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, Pressable, ScrollView, Text, TextInput, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 import { fetchWordDetails } from './api/dictionary';
 import { fetchRandomWords } from './api/randomWords';
 import { WordDetails } from './types/dictionary';
+
+import LoadingIndicator from '~/components/LoadingIndicator';
+import SearchBar from '~/components/SearchBar';
+import WordCard from '~/components/WordCard';
 
 export default function Home() {
   const router = useRouter();
@@ -16,7 +20,8 @@ export default function Home() {
   useEffect(() => {
     const getRandomWordDetails = async () => {
       try {
-        const randomWords = await fetchRandomWords(7);
+        const randomWords = await fetchRandomWords(12);
+        console.log('randomWords:', randomWords);
         const detailsPromises = randomWords.map((word) => fetchWordDetails(word));
         const details: (WordDetails | null)[] = await Promise.all(detailsPromises);
 
@@ -26,9 +31,8 @@ export default function Home() {
       } catch (error) {
         console.error('Error fetching random words details:', error);
         setError('Failed to load words. Please try again later.');
-      } finally {
-        setIsLoading(false);
       }
+      setIsLoading(false);
     };
 
     getRandomWordDetails();
@@ -41,23 +45,9 @@ export default function Home() {
         <View className="flex-row items-center justify-end">
           <Ionicons name="scan" size={24} color="#999" />
         </View>
-
-        <Pressable className="mb-4" onPress={() => router.push('/search')}>
-          <View className="mt-4 w-full flex-row items-center rounded-lg bg-[#E7E7E7] px-4">
-            <Ionicons name="search" size={24} color="#999" />
-            <TextInput
-              className="ml-2 h-14 flex-1 text-[#282C34]"
-              placeholder="Find the meaning..."
-              placeholderTextColor="#999"
-              editable={false}
-            />
-          </View>
-        </Pressable>
-
+        <SearchBar value="" onChange={() => {}} onPress={() => router.push('/search')} />
         {isLoading ? (
-          <View className="mt-7 flex-1 items-center justify-center">
-            <ActivityIndicator size="large" color="#2563eb" />
-          </View>
+          <LoadingIndicator />
         ) : error ? (
           <View className="mt-7 flex-1 items-center justify-center">
             <Text className="text-lg text-[#666]">{error}</Text>
@@ -67,47 +57,7 @@ export default function Home() {
             <View>
               <Text className="my-7 text-2xl font-bold">Discover new words</Text>
               {randomWordDetails.map((wordDetails, idx) => (
-                <Link
-                  key={idx}
-                  href={{ pathname: '/details', params: { word: wordDetails.word } }}
-                  asChild>
-                  <Pressable>
-                    {({ pressed }) => (
-                      <View
-                        className={`center mb-7 flex-row rounded-lg bg-[#FAF6F3] p-7 ${pressed ? 'opacity-50' : ''}`}>
-                        <View className="flex-col">
-                          <View className="flex-row items-center gap-2">
-                            <View className="h-7 w-2 bg-[#2563eb]" />
-                            <Text className="text-4xl font-bold">
-                              {wordDetails.word.charAt(0).toUpperCase() + wordDetails.word.slice(1)}
-                            </Text>
-                            <Text className="text-[#999]">
-                              {wordDetails.meanings[0]?.partOfSpeech || 'Unknown'}
-                            </Text>
-                          </View>
-                          {wordDetails.phonetics?.[0]?.audio && wordDetails.phonetics?.[0].text && (
-                            <Text className="py-1 text-[1.2rem] italic text-[#999]">
-                              {wordDetails.phonetics?.[0]?.text || '/.../'}
-                            </Text>
-                          )}
-                          <Text>
-                            {wordDetails.meanings[0]?.definitions[0]?.definition ||
-                              'No definition available.'}
-                          </Text>
-                          {wordDetails.meanings?.[0]?.definitions?.[0]?.example && (
-                            <Text className="mt-2 italic text-[#666]">
-                              Example:{' '}
-                              {wordDetails.meanings?.[0]?.definitions?.[0]?.example
-                                .charAt(0)
-                                .toUpperCase() +
-                                wordDetails.meanings?.[0]?.definitions?.[0]?.example.slice(1)}
-                            </Text>
-                          )}
-                        </View>
-                      </View>
-                    )}
-                  </Pressable>
-                </Link>
+                <WordCard key={idx} wordDetails={wordDetails} />
               ))}
             </View>
           </ScrollView>
